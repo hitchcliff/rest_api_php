93e0ae21230233fd746be539413b3c0bebe88140
kevin700brands <kevin@700brands.com>
2022-08-16 07:29:24 +0800

    Read  posts from DB

--- config/Database.php
+++ config/Database.php
@@ -5,7 +5,7 @@ class Database
   private $host = 'localhost';
   private $db_name = 'myblog';
   private $username = 'root';
-  private $password = '123456';
+  private $password = '';
   private $conn;
 
   // DB Connect
